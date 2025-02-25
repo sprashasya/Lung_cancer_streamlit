4908b42aa75e592b90dfced4f53f2584e505ee88
sprashasya <prashasyasrivastava@MacBookAir.lan>
2025-02-25 20:16:43 +0530

    streamlit_python_script

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,20 @@ import pandas as pd
 import numpy as np
 import pickle
 
+# Load the pre-trained scaler and model
 scaler = pickle.load(open("scaler.pkl", "rb"))
 model = pickle.load(open("best_logistic_regression.pkl", "rb"))
 
-
 st.write("""
-# Lung Cancer Prediction App
+# Lung Cancer Prediction 
 This app predicts the **Possibility of Lung Cancer** based on user inputs!
 """)
 
 st.sidebar.header("User Input Features")
 
-
+# Function to get manual user input
 def user_input_features():
-    GENDER = st.sidebar.selectbox("Gender", ("Male", "Female"))
+    GENDER = st.sidebar.selectbox("Gender", ("Male", "Female","Others"))
     AGE = st.sidebar.slider("Age", 20, 90, 50)
     
     options = {"No": 1, "Yes": 2}  
@@ -56,9 +56,21 @@ def user_input_features():
     features = pd.DataFrame(data, index=[0])
     return features
 
+# CSV Upload
 
-input_df = user_input_features()
+uploaded_file = st.sidebar.file_uploader("Upload CSV File", type=["csv"])
+if st.sidebar.button("View Example Data"):
+    example_data = pd.read_csv("Lung_cancer_detection.csv")  # Ensure correct file path
+    st.subheader("Example Data")
+    st.write(example_data.head(10))  # Show first 10 rows
 
+
+
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+    st.sidebar.success("CSV uploaded successfully!")
+else:
+    input_df = user_input_features()
 
 feature_names = [
     "GENDER", "AGE", "SMOKING", "YELLOW_FINGERS", "ANXIETY", "PEER_PRESSURE",
@@ -66,25 +78,23 @@ feature_names = [
     "COUGHING", "SHORTNESS OF BREATH", "SWALLOWING DIFFICULTY", "CHEST PAIN"
 ]
 
+# Scaling the input
+data_scaled = scaler.transform(input_df)
 
-input_scaled = scaler.transform(pd.DataFrame(input_df, columns=feature_names))
-
-
-prediction = model.predict(input_scaled)
-prediction_proba = model.predict_proba(input_scaled)
-
+# Making predictions
+predictions = model.predict(data_scaled)
+predictions_proba = model.predict_proba(data_scaled)
 
 st.subheader("User Input Features")
 st.write(input_df)
 
-
-st.subheader("Prediction")
-st.write("Lung Cancer Risk: **Yes**" if prediction[0] == 1 else "Lung Cancer Risk: **No**")
-
+st.subheader("Predictions")
+input_df["Lung Cancer Risk"] = ["Yes" if pred == 1 else "No" for pred in predictions]
+st.write(input_df[["Lung Cancer Risk"]])
 
 st.subheader("Prediction Probability")
 probability_df = pd.DataFrame({
     "Lung Cancer": ["No", "Yes"],
-    "Probability": [f"{prediction_proba[0][0]:.2f}", f"{prediction_proba[0][1]:.2f}"]
+    "Probability": [f"{predictions_proba[0][0]:.2f}", f"{predictions_proba[0][1]:.2f}"]
 })
 st.table(probability_df)
